e98963fdb812a69c8d43a876af17bc4bf219a42c
NikolosD <alcari2002@gmail.com>
2023-10-02 18:37:07 +0400

    hw3

--- hws2-master/src/s2-homeworks/hw03/GreetingContainer.tsx
+++ hws2-master/src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -59,8 +59,8 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
         pureOnEnter(e, addUser)
     }
 
-    const totalUsers = users.length // need to fix
-    const lastUserName = users.length > 0 ? users[users.length - 1].name : '' // need to fix
+    const totalUsers = users?.length // need to fix
+    const lastUserName = users[users.length - 1]?.name // need to fix
 
     return (
         <Greeting
